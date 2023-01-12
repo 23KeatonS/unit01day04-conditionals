1291721a86bae6870e6a94fd27571f16a41920a7
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-01-12 11:32:04 -0600

    added ThreeEqual

--- ThreeEqual.java
+++ ThreeEqual.java
@@ -1,5 +1,16 @@
 public class ThreeEqual {
     public static void main(String[] args) {
-        
+        if (args.length<3){
+            System.out.println("Please Provide at least 3 integer arguments.");
+        }else{
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int c = Integer.parseInt(args[2]);
+            if (a == b && b == c){
+                System.out.println("Equal");
+            }else{
+                System.out.println("Not Equal");
+            }
+        }
     }
 }
