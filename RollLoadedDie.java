df854c757c2a011e85abc5223106e4517ca65ae2
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-01-17 16:20:11 -0600

    changed RollLoadedDie(hopefully fixed)

--- RollLoadedDie.java
+++ RollLoadedDie.java
@@ -2,8 +2,8 @@
 public class RollLoadedDie {
     public static void main(String[] args) {
         double randnum = Math.random();
-        int maxval = 7;
-        double num = Math.round(maxval*randnum+1);
+        int maxval = 8;
+        double num = Math.floor(maxval*randnum+1);
         if (num>=6){
             num = 6;
             System.out.println(num);
